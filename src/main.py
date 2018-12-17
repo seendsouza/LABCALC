36ef531d14ac887a707d67c4650e0cf7f4819955
seendsouza <sdammobubbles@gmail.com>
2018-12-17 16:37:05 -0500

    made the lexer work and remade the language so that an equ is a class

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 import lexer
 def main():
-# Read the LABCALC source code and store it in a variable
+    """
+    Read the LABCALC source code and store it in a variable
+    """
     content = ""
-    with open('src/test.lang','r') as file:
+    with open('./test.lang','r') as file:
     #
     # Lexer
     #
-    while readlineTrue:
-        line = file.readline()
-        # Call and initialize lexer class
-        linestring = ''.join(map(str, line))
-        lex = lexer.Lexer(linestring)
-        # Call tokenize method
-        tokens = lex.tokenize()
-        if not line:
-            break
+        while True:
+            line = file.readline()
+            # Call and initialize lexer class
+            linestring = ''.join(map(str, line))
+            lex = lexer.Lexer(linestring)
+            # Call tokenize method
+            tokens = lex.tokenize()
+            if not line:
+                break
 
 main()
